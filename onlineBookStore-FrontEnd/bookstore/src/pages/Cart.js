4a7f0591cb2852acdf1be86bf814e5380cc60efb
DilankaSandeep <dilankacm@gmail.com>
2023-09-24 16:24:36 +0530

    added css to Cart page

--- onlineBookStore-FrontEnd/bookstore/src/pages/Cart.js
+++ onlineBookStore-FrontEnd/bookstore/src/pages/Cart.js
@@ -2,18 +2,21 @@ import React from 'react';
 import { useCart } from '../CartContex';
 import { useState, useEffect } from 'react';
 import Table from 'react-bootstrap/Table';
+import { Button } from 'react-bootstrap';
 
 const Cart = () => {
   const [cart, setCart] = useState([]);
   const { removeFromCart } = useCart();
-  const [qnty, setQuty] = useState(1);
-  const [subtotal, setSubTotal] =useState(0);
   const [total, setTotal] =useState(0);
+  const [orderQuantities, setOrderQuantities] = useState({});
   
   useEffect(() => {
-    const cartTotal = cart.reduce((acc, item) => acc + item.price, 0);
+    const cartTotal = cart.reduce((acc, item) => {
+      const orderQuantity = orderQuantities[item.id] || 1; 
+      return acc + item.price * orderQuantity;
+    }, 0);
     setTotal(cartTotal);
-  }, [cart]);
+  }, [cart, orderQuantities]);
 
   useEffect(() => {
     const savedCart = localStorage.getItem('cart');
@@ -21,10 +24,25 @@ const Cart = () => {
       setCart(JSON.parse(savedCart));
     }
   }, []);
+    const increaseOrderQuantity = (itemId) => {
+      setOrderQuantities((prevOrderQuantities) => ({
+        ...prevOrderQuantities,
+        [itemId]: (prevOrderQuantities[itemId] || 0) + 1,
+      }));
+    };
+  
+
+    const decreaseOrderQuantity = (itemId) => {
+      setOrderQuantities((prevOrderQuantities) => ({
+        ...prevOrderQuantities,
+        [itemId]: Math.max((prevOrderQuantities[itemId] || 0) - 1, 1), 
+      }));
+    };
+
 
   return (
     <div className="cart">
-      <h2>Shopping Cart</h2>
+      <h2>Your Cart</h2>
       <Table striped bordered hover>
         <thead>
           <tr>
@@ -41,13 +59,15 @@ const Cart = () => {
         <tbody>
           {cart && cart.map((item, index) => (
             <tr key={item.id}>
-              <td>{index + 1}<div>{ <button onClick={() => removeFromCart(item.id)}>Remove ❌</button>}</div></td>
+              <td>{index + 1}<div>{ <Button variant="outline-danger" onClick={() => removeFromCart(item.id)}>❌</Button>}</div></td>
               <td>{<div>{item.title}</div>}{<div>{<img src={require(`../imges/${item.imageUrl}`)} height={40} width={25} alt={item.title} />}</div>}</td>
               <td>Rs.{item.price}</td>
               <td>Category: {item.category.categoryName}<div>subCategory: {item.subCategory.subCategoryName}</div></td>
-              <td>{qnty}</td>
+              <td> <Button variant="outline-warning" className ="decreaseButton" onClick={() => decreaseOrderQuantity(item.id)}>-</Button>
+                {orderQuantities[item.id] || 1}
+                <Button variant="outline-primary" onClick={() => increaseOrderQuantity(item.id)}>+</Button></td>
 
-              <td>{qnty*item.price}</td>
+              <td>{(item.price * (orderQuantities[item.id] || 1)).toFixed(2)}</td>
             </tr>
           ))
 
@@ -59,7 +79,9 @@ const Cart = () => {
           </tr>
         </tbody>
       </Table>
+      <div className='plaseOrder'><Button variant='primary'>place Order</Button></div>
     </div>
+
   );
 };
 
